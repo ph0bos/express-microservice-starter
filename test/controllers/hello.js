0cc18ce26bf0728e96abead63f6b9a4e774e644b
Steven Robinson <sm0k3yuk@gmail.com>
2015-05-12 16:10:47 +0100

    Basic functionality

---
+++ test/controllers/hello.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Hello Controller
+ */
+module.exports = function (router) {
+
+  router.get('/', function (req, res, next) {
+    res.json({
+      "message": "Hello"
+    });
+  });
+};
