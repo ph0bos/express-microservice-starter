affc5f3277d18c364eb9e9fbff1688bd25ee6cb7
Steven Robinson <sm0k3yuk@gmail.com>
2015-05-12 22:57:13 +0100

    Default plus config overlay options

--- index.js
+++ index.js
@@ -15,17 +15,27 @@ var log       = require('bunyan').createLogger({ name: 'microservice' });
 var buildOptions = function(options) {
   var config = konfig({ path: options.configPath || './config' });
 
+  // Default options
   options                 = options || {};
+  options.server          = options.server || { port: process.env.PORT };
   options.debug           = options.debug || false;
-  options.server          = config.app.server || { port: 8000 };
-
-  options.serviceName     = config.app.microservice.server.name || pkgInfo.name;
-  options.serviceBasePath = config.app.microservice.basePath || 'services';
-  
-  options.zookeeper       = config.app.zookeeper || { connectionString: 'localhost:2181', retry: { count: 5 } };
   options.discoverable    = options.discoverable || false;
   options.routesPath      = options.routesPath || 'controllers';
   options.callerPath      = path.dirname(caller(2));
+  options.serviceName     = pkgInfo.name;
+  options.serviceBasePath = 'services';
+  options.zookeeper       = { connectionString: 'localhost:2181', retry: { count: 5 } };
+
+  // Return now if we have no config
+  if (!config.app) {
+    return options;
+  }
+
+  // Overlay config file options
+  options.server          = config.app.server || { port: process.env.PORT };
+  options.serviceName     = config.app.microservice.server.name || pkgInfo.name;
+  options.serviceBasePath = config.app.microservice.basePath || 'services';
+  options.zookeeper       = config.app.zookeeper || { connectionString: 'localhost:2181', retry: { count: 5 } };
   
   return options;
 };
