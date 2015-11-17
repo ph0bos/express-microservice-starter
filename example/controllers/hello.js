cb29f2372d0d03fadb2c4406c4b3c034b26301d1
Steven Robinson <sm0k3yuk@gmail.com>
2015-11-17 16:14:40 +0000

    Add express-validator to bootstrap

--- example/controllers/hello.js
+++ example/controllers/hello.js
@@ -1,12 +1,35 @@
 'use strict';
 
+var util = require('util');
+
 /**
  * Hello Controller
  */
 module.exports = function (router) {
+
+  /**
+   * Sample GET
+   */
   router.get('/', function (req, res, next) {
     res.json({
       "message": "Hello"
+    });
+  });
+
+  /**
+   * Sample GET with validation
+   */
+  router.get('/is/it/me/youre/looking/for', function (req, res, next) {
+    req.checkQuery('limit', 'Invalid limit parameter').optional().isInt();
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+      return res.status(400).json(errors);
+    }
+
+    res.json({
+      "message": "Hello Lionel"
     });
   });
 };
