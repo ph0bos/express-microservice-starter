293ce7d9f362cf952dd853add35fb95f4deebf73
Steven Robinson <steven@ph0bos.io>
2017-08-13 22:41:25 +0100

    Add app.locals microservice data and event emitters on service registration and zookeeper disconnection

--- example/index.js
+++ example/index.js
@@ -15,6 +15,10 @@ app.use(micro({
   monitorsPath: './monitors'
 }));
 
-app.listen(8000, function onListen() {
-  console.log('example app initialised and serving at the following root: http://localhost:8000/starter/v1');
+app.on('service:registered', function (data) {
+  console.log('example app registered with zookeeper', data);
+
+  app.listen(8000, function onListen() {
+    console.log('example app initialised and serving at the following root: http://localhost:8000/starter/v1');
+  });
 });
